f59a2c99cc9953ef452b1928ce996dba34f08c6e
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 17:11:23 -0300

    atualiza user controller com operações de getall e create (IDE acusando erro em @RequiredArgs...)

--- users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
+++ users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
@@ -1,15 +1,36 @@
 package br.edu.ufop.web.users.controller;
 
+import br.edu.ufop.web.users.dto.CreateUserDTO;
+import br.edu.ufop.web.users.dto.UserDTO;
+import br.edu.ufop.web.users.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/status")
     public ResponseEntity<String> getStatus() {
         return ResponseEntity.ok("User service is running");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody CreateUserDTO createUserDTO) {
+        return ResponseEntity.ok(userService.create(createUserDTO));
     }
 }
