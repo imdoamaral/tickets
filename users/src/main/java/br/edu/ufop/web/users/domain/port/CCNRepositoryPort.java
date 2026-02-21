86cf6eca48294955add9ebc4a6acec2b80144f5e
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 17:28:38 -0300

    adiciona ccn repository port

---
+++ users/src/main/java/br/edu/ufop/web/users/domain/port/CCNRepositoryPort.java
@@ -0,0 +1,8 @@
+package br.edu.ufop.web.users.domain.port;
+
+import br.edu.ufop.web.users.domain.CCNetworkDomain;
+
+import java.util.UUID;
+
+public interface CCNRepositoryPort extends RepositoryPort<CCNetworkDomain, UUID>{
+}
