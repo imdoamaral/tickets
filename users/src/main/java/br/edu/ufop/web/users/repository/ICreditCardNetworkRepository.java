a9f72a9bc103fd5002ab544caefc80387547550a
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 13:36:09 -0300

    atualiza arquivo de repository com novas queries

--- users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
+++ users/src/main/java/br/edu/ufop/web/users/repository/ICreditCardNetworkRepository.java
@@ -2,11 +2,21 @@ package br.edu.ufop.web.users.repository;
 
 import br.edu.ufop.web.users.entity.CCNetworkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ICreditCardNetworkRepository extends JpaRepository<CCNetworkEntity, UUID> {
     List<CCNetworkEntity> findByName(String name);
+
     List<CCNetworkEntity> findByNameContainingIgnoreCase(String name);
+
+    @Query(value = "SELECT cce FROM CCNetworkEntity  cce " + "WHERE cce.tax = :tax")
+    Optional<CCNetworkEntity> procurarPorTaxa(@Param("tax") Double tax);
+
+    @Query(value = "select * from tb_credit_card_network " + "where tax = :tax ", nativeQuery = true)
+    Optional<CCNetworkEntity> procurarPorTaxaSQLNativo(@Param("tax") Double tax);
 }
