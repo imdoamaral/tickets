99dada4910967ed2596373ee1008429792929473
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 12:52:25 -0300

    adiciona ccn converter

---
+++ users/src/main/java/br/edu/ufop/web/users/converter/CCNetworkConverter.java
@@ -0,0 +1,34 @@
+package br.edu.ufop.web.users.converter;
+
+import br.edu.ufop.web.users.domain.CCNetworkDomain;
+import br.edu.ufop.web.users.dto.CreateCreditCardNetworkDTO;
+import br.edu.ufop.web.users.dto.CreditCardNetworkDTO;
+import br.edu.ufop.web.users.entity.CCNetworkEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CCNetworkConverter {
+    // DTO de entrada para classe de dominio
+    public static CCNetworkDomain toDomain(CreateCreditCardNetworkDTO createCreditCardNetworkDTO) {
+        return CCNetworkDomain.builder()
+                .name(createCreditCardNetworkDTO.name())
+                .build();
+    }
+
+    // Classe de dominio para entidade JPA
+    public static CCNetworkEntity toEntity(CCNetworkDomain ccNetworkDomain) {
+        return CCNetworkEntity.builder()
+                .id(ccNetworkDomain.getId())
+                .name(ccNetworkDomain.getName())
+                .build();
+    }
+
+    // Entidade JPA para DTO de saida
+    public CreditCardNetworkDTO toDto(CCNetworkEntity ccNetworkEntity) {
+        return new CreditCardNetworkDTO(
+                ccNetworkEntity.getId(),
+                ccNetworkEntity.getName()
+        );
+    }
+}
