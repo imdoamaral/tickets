075d9f0dd56d0d77087df983397d4ebdef095bbc
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 13:28:11 -0300

    atualiza CCNetworkEntity com novos atributos

--- users/src/main/java/br/edu/ufop/web/users/entity/CCNetworkEntity.java
+++ users/src/main/java/br/edu/ufop/web/users/entity/CCNetworkEntity.java
@@ -23,4 +23,8 @@ public class CCNetworkEntity {
 
     @Column(nullable = false, unique = true, columnDefinition = "varchar(100)")
     private String name;
+
+    private String comment;
+
+    private Double tax;
 }
