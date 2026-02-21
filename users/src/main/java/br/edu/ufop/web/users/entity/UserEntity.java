bbeb2b02ca73f2a0c4c3f5d9b4e48d474713840a
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-21 11:42:10 -0300

    adiciona user entity

---
+++ users/src/main/java/br/edu/ufop/web/users/entity/UserEntity.java
@@ -0,0 +1,56 @@
+package br.edu.ufop.web.users.entity;
+
+import br.edu.ufop.web.users.enums.EnumUserType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="tb_users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String city;
+
+    private String creditCardNumber;
+
+    // credit card network
+
+    private EnumUserType type;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private Boolean active;
+
+    @PrePersist
+    public void beforeSave() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.active = true;
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
