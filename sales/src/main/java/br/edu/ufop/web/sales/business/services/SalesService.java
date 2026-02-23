b8f4f74918b8ca873aaddfc1d019a50bb0bdbb1e
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 02:10:14 -0300

    adiciona: sale service

---
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/SalesService.java
@@ -0,0 +1,31 @@
+package br.edu.ufop.web.sales.business.services;
+
+import br.edu.ufop.web.sales.business.converters.SaleConverter;
+import br.edu.ufop.web.sales.controller.dtos.events.CreateSaleDTO;
+import br.edu.ufop.web.sales.controller.dtos.events.SaleDTO;
+import br.edu.ufop.web.sales.infraestructure.entities.SaleEntity;
+import br.edu.ufop.web.sales.infraestructure.repositories.ISaleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SalesService {
+    private final ISaleRepository saleRepository;
+
+    public List<SaleDTO> getAll() {
+        List<SaleEntity> saleEntityList = saleRepository.findAll();
+
+        return saleEntityList.stream()
+                .map(SaleConverter::toDTO)
+                .toList();
+    }
+
+    public SaleDTO create(CreateSaleDTO createSaleDTO) {
+        SaleEntity eventEntity = SaleConverter.toEntity(createSaleDTO);
+        eventEntity = saleRepository.save(eventEntity);
+        return SaleConverter.toDTO(eventEntity);
+    }
+}
