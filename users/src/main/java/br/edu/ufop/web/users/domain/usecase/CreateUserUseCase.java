467460df983b4f9354ac54a5529aa2279adc90ac
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-22 21:11:30 -0300

    atualiza o tratamento de exceção customizado e adiciona outras validações

--- users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
+++ users/src/main/java/br/edu/ufop/web/users/domain/usecase/CreateUserUseCase.java
@@ -3,11 +3,14 @@ package br.edu.ufop.web.users.domain.usecase;
 import br.edu.ufop.web.users.domain.CCNetworkDomain;
 import br.edu.ufop.web.users.domain.UserDomain;
 import br.edu.ufop.web.users.domain.port.CCNRepositoryPort;
+import br.edu.ufop.web.users.enums.EnumUserType;
+import br.edu.ufop.web.users.exception.UseCaseException;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,11 +27,14 @@ public class CreateUserUseCase {
 
         // Demais validaçoes
         validateCCNetworkId();
+        validateEmail();
+        validatePassword();
+        validateUserType();
     }
 
     private void validateName() {
         if (this.userDomain.getName() == null) {
-            throw new RuntimeException("Name is null.");
+            throw new UseCaseException("Name is null.");
         }
     }
 
@@ -40,15 +46,33 @@ public class CreateUserUseCase {
         UUID id = this.userDomain.getCcNetwork().getId();
 
         if (id == null) {
-            throw new RuntimeException("Credit Card Network ID is null.");
+            throw new UseCaseException("Credit Card Network ID is null.");
         }
 
         Optional<CCNetworkDomain> domainOptional = repositoryPort.findById(id);
 
         if (domainOptional.isEmpty()) {
-            throw new RuntimeException("Credit Card Network ID does not exist.");
+            throw new UseCaseException("Credit Card Network ID does not exist.");
         }
 
         this.userDomain.setCcNetwork(domainOptional.get());
+    }
+
+    private void validateEmail() {
+        if (this.userDomain.getEmail() == null) {
+            throw new UseCaseException("E-mail is null.");
+        }
+    }
+
+    private void validatePassword() {
+        if (this.userDomain.getPassword() == null) {
+            throw new UseCaseException("Password is null.");
+        }
+    }
+
+    private void validateUserType() {
+        if (Objects.isNull(this.userDomain.getType())) {
+            this.userDomain.setType(EnumUserType.CUSTOMER);
+        }
     }
 }
