f88a64c2b09881629ccdbbe838ed4155d2ceec8a
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 02:12:24 -0300

    refatora: nome da classe

--- sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class SalesService {
+public class SaleService {
     private final ISaleRepository saleRepository;
 
     public List<SaleDTO> getAll() {
