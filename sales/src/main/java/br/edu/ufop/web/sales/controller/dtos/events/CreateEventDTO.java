72c4005d3c3c6c1f2a5fa6fe050fc98f538d9425
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 01:37:37 -0300

    adiciona create event DTO

---
+++ sales/src/main/java/br/edu/ufop/web/sales/controller/dtos/events/CreateEventDTO.java
@@ -0,0 +1,24 @@
+package br.edu.ufop.web.sales.controller.dtos.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateEventDTO {
+    private String description;
+    private Integer type;
+
+    private LocalDateTime dateTime;
+
+    private LocalDateTime startingSales;
+    private LocalDateTime endingSales;
+
+    private Float price;
+}
