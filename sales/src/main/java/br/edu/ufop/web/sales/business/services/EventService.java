a0cd6027e758ca9e184dfbb1d0061bf7a2898392
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 01:57:15 -0300

    adiciona event service - get all & create

---
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/EventService.java
@@ -0,0 +1,31 @@
+package br.edu.ufop.web.sales.business.services;
+
+import br.edu.ufop.web.sales.business.converters.EventConverter;
+import br.edu.ufop.web.sales.controller.dtos.events.CreateEventDTO;
+import br.edu.ufop.web.sales.controller.dtos.events.EventDTO;
+import br.edu.ufop.web.sales.infraestructure.entities.EventEntity;
+import br.edu.ufop.web.sales.infraestructure.repositories.IEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final IEventRepository eventRepository;
+
+    public List<EventDTO> getAll() {
+        List<EventEntity> eventEntityList = eventRepository.findAll();
+
+        return eventEntityList.stream()
+                .map(EventConverter::toDTO)
+                .toList();
+    }
+
+    public EventDTO create(CreateEventDTO createEventDTO) {
+        EventEntity eventEntity = EventConverter.toEntity(createEventDTO);
+        eventEntity = eventRepository.save(eventEntity);
+        return EventConverter.toDTO(eventEntity);
+    }
+}
