39e900331560a05ff8ed11513913e21dbd1f086e
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-22 23:02:28 -0300

    atualiza com update e delete user

--- users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
+++ users/src/main/java/br/edu/ufop/web/users/controller/UserController.java
@@ -1,6 +1,8 @@
 package br.edu.ufop.web.users.controller;
 
 import br.edu.ufop.web.users.dto.CreateUserDTO;
+import br.edu.ufop.web.users.dto.DeleteUserDTO;
+import br.edu.ufop.web.users.dto.UpdateUserDTO;
 import br.edu.ufop.web.users.dto.UserDTO;
 import br.edu.ufop.web.users.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -41,5 +43,30 @@ public class UserController {
         }
 
         return ResponseEntity.ok(userDTOOptional.get());
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<UserDTO>> getByName(@PathVariable(value = "name") String name) {
+        List<UserDTO> userDTOList = userService.getByName(name);
+
+        return userDTOList.isEmpty() ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(userDTOList);
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UpdateUserDTO updateUserDTO) {
+        UserDTO userDTO = userService.updateUser(updateUserDTO);
+        return ResponseEntity.ok(userDTO);
+    }
+
+    // put email, password (antiga e nova), numero do cartao de credito
+    // bandeira do cartao de credito
+
+    // Delete
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@RequestBody DeleteUserDTO deleteUserDTO) {
+        userService.delete(deleteUserDTO);
+        return ResponseEntity.ok().build();
     }
 }
--- users/src/main/java/br/edu/ufop/web/users/service/UserService.java
+++ users/src/main/java/br/edu/ufop/web/users/service/UserService.java
@@ -4,8 +4,11 @@ import br.edu.ufop.web.users.converter.UserConverter;
 import br.edu.ufop.web.users.domain.UserDomain;
 import br.edu.ufop.web.users.domain.usecase.CreateUserUseCase;
 import br.edu.ufop.web.users.dto.CreateUserDTO;
+import br.edu.ufop.web.users.dto.DeleteUserDTO;
+import br.edu.ufop.web.users.dto.UpdateUserDTO;
 import br.edu.ufop.web.users.dto.UserDTO;
 import br.edu.ufop.web.users.entity.UserEntity;
+import br.edu.ufop.web.users.exception.UseCaseException;
 import br.edu.ufop.web.users.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -56,5 +59,49 @@ public class UserService {
 
         UserDTO userDTO = UserConverter.toUserDTO(userEntityOptional.get());
         return Optional.of(userDTO);
+    }
+
+    public List<UserDTO> getByName(String name) {
+        List<UserEntity> userEntityList = repository.findAllByNameContainingIgnoreCase(name);
+
+        return userEntityList.stream()
+                .map(UserConverter::toUserDTO)
+                .toList();
+    }
+
+    public UserDTO updateUser(UpdateUserDTO updateUserDTO) {
+        // UseCase -> porta: domain para entity
+        // Identificar se o ID é valido
+        Optional<UserEntity> userEntityOptional = repository.findById(updateUserDTO.id());
+
+        if (userEntityOptional.isEmpty()) {
+            throw new UseCaseException("User ID does not found.");
+        }
+
+        UserEntity userEntity = userEntityOptional.get();
+
+        // update -> use case
+        userEntity.setName(updateUserDTO.name());
+        userEntity.setCity(updateUserDTO.city());
+
+        repository.save(userEntity);
+
+        return UserConverter.toUserDTO(userEntity);
+    }
+
+    public void delete(DeleteUserDTO deleteUserDTO) {
+        Optional<UserEntity> userEntityOptional= repository.findById(deleteUserDTO.id());
+
+        if (userEntityOptional.isEmpty()) {
+            throw new UseCaseException("User ID was not found.");
+        }
+
+        UserEntity userEntity = userEntityOptional.get();
+
+        if (!userEntity.getPassword().equals(deleteUserDTO.password())) {
+            throw new UseCaseException("Invalid password.");
+        }
+
+        repository.delete(userEntity);
     }
 }
