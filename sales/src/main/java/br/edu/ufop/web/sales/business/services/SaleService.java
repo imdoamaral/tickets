9803479785a44d1f5fbb9476aa39e82f4bbd00fb
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 11:00:02 -0300

    atualiza: validate event

--- sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
@@ -3,17 +3,21 @@ package br.edu.ufop.web.sales.business.services;
 import br.edu.ufop.web.sales.business.converters.SaleConverter;
 import br.edu.ufop.web.sales.controller.dtos.events.CreateSaleDTO;
 import br.edu.ufop.web.sales.controller.dtos.events.SaleDTO;
+import br.edu.ufop.web.sales.infraestructure.entities.EventEntity;
 import br.edu.ufop.web.sales.infraestructure.entities.SaleEntity;
 import br.edu.ufop.web.sales.infraestructure.repositories.ISaleRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
     private final ISaleRepository saleRepository;
+    private final EventService eventService;
 
     public List<SaleDTO> getAll() {
         List<SaleEntity> saleEntityList = saleRepository.findAll();
@@ -23,9 +27,19 @@ public class SaleService {
                 .toList();
     }
 
+    @Transactional
     public SaleDTO create(CreateSaleDTO createSaleDTO) {
-        SaleEntity eventEntity = SaleConverter.toEntity(createSaleDTO);
-        eventEntity = saleRepository.save(eventEntity);
-        return SaleConverter.toDTO(eventEntity);
+        SaleEntity saleEntity = SaleConverter.toEntity(createSaleDTO);
+
+        Optional<EventEntity> eventEntityOptional = eventService.getById(createSaleDTO.getEventId());
+
+        if (eventEntityOptional.isEmpty()) {
+            throw new RuntimeException("Event does not exist.");
+        }
+
+        saleEntity.setEvent(eventEntityOptional.get());
+
+        saleRepository.save(saleEntity);
+        return SaleConverter.toDTO(saleEntity);
     }
 }
