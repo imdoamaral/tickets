dd1ad320bb559cac994e675a741691097870240d
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-22 21:05:51 -0300

    adiciona exception customizado

---
+++ users/src/main/java/br/edu/ufop/web/users/exception/UseCaseException.java
@@ -0,0 +1,7 @@
+package br.edu.ufop.web.users.exception;
+
+public class UseCaseException extends RuntimeException {
+    public UseCaseException(String message) {
+        super(message);
+    }
+}
