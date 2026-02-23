3510ba0d653004bf9d344576efe2a8f4d718acac
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 11:02:03 -0300

    adiciona: openfeign - user client

---
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/clients/UserServiceClient.java
@@ -0,0 +1,17 @@
+package br.edu.ufop.web.sales.business.services.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient(name = "users-service", url = "http://localhost3000")
+public interface UserServiceClient {
+    @GetMapping("/users")
+    public List<UserServiceDTO> getAllUsers();
+
+    @GetMapping("/users/{id}")
+    public UserServiceDTO getById(@PathVariable(value = "id")UUID id);
+}
