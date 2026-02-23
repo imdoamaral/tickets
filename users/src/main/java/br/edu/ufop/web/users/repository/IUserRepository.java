a5814c1048072e2e9fe13e0e9c6b743f46d5a8dd
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-22 22:39:52 -0300

    atualiza com query by name

--- users/src/main/java/br/edu/ufop/web/users/repository/IUserRepository.java
+++ users/src/main/java/br/edu/ufop/web/users/repository/IUserRepository.java
@@ -2,8 +2,18 @@ package br.edu.ufop.web.users.repository;
 
 import br.edu.ufop.web.users.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByName(String name);
+    List<UserEntity> findAllByNameContainingIgnoreCase(String name);
+
+    @Query(value = "SELECT * FROM tb_users u WHERE u.name LIKE %:name%", nativeQuery = true)
+    List<UserEntity> findAllNameLike(@Param("name") String name);
 }
