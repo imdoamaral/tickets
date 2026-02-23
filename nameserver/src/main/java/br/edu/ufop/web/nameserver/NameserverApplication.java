1c230625ced9c60e1ea909411aff9174b5334d13
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-23 13:26:35 -0300

    funcionalidade: ✨ adiciona módulo nameserver

---
+++ nameserver/src/main/java/br/edu/ufop/web/nameserver/NameserverApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufop.web.nameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameserverApplication.class, args);
+    }
+
+}
