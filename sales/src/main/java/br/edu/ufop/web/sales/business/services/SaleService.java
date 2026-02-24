669eb57a13ba476faa51e0782b0129de8d2e14bd
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-24 20:50:11 -0300

    atualiza: update & delete

--- sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
+++ sales/src/main/java/br/edu/ufop/web/sales/business/services/SaleService.java
@@ -2,7 +2,9 @@ package br.edu.ufop.web.sales.business.services;
 
 import br.edu.ufop.web.sales.business.converters.SaleConverter;
 import br.edu.ufop.web.sales.controller.dtos.events.CreateSaleDTO;
+import br.edu.ufop.web.sales.controller.dtos.events.DeleteSaleDTO;
 import br.edu.ufop.web.sales.controller.dtos.events.SaleDTO;
+import br.edu.ufop.web.sales.controller.dtos.events.UpdateSaleDTO;
 import br.edu.ufop.web.sales.infraestructure.entities.EventEntity;
 import br.edu.ufop.web.sales.infraestructure.entities.SaleEntity;
 import br.edu.ufop.web.sales.infraestructure.repositories.ISaleRepository;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -41,5 +44,31 @@ public class SaleService {
 
         saleRepository.save(saleEntity);
         return SaleConverter.toDTO(saleEntity);
+    }
+
+    public SaleDTO getById(UUID id) {
+        SaleEntity saleEntity = saleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sale not found with id: " + id));
+
+        return SaleConverter.toDTO(saleEntity);
+    }
+
+    @Transactional
+    public SaleDTO update(UUID id, UpdateSaleDTO updateSaleDTO) {
+        SaleEntity saleEntity = saleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sale not found with id: " + id));
+
+        SaleConverter.updateEntity(saleEntity, updateSaleDTO);
+        saleEntity = saleRepository.save(saleEntity);
+
+        return SaleConverter.toDTO(saleEntity);
+    }
+
+    @Transactional
+    public void delete(DeleteSaleDTO deleteSaleDTO) {
+        SaleEntity saleEntity = saleRepository.findById(deleteSaleDTO.id())
+                .orElseThrow(() -> new RuntimeException("Sale not found with id: " + deleteSaleDTO.id()));
+
+        saleRepository.delete(saleEntity);
     }
 }
