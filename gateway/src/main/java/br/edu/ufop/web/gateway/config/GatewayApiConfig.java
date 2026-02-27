6b66daac187bac8af94686a16b1303316f2ab55e
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 15:07:22 -0300

    refatora: gateway api config

--- gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
+++ gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
@@ -1,6 +1,5 @@
 package br.edu.ufop.web.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,39 +7,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayApiConfig {
-    @Value("${gateway.frontend.uri}")
-    private String frontEndUri;
+
+    private String frontEndUri = "http://localhost:5173";
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("users-api",
-                        pred -> pred.path("/api/users/**")
-                                .filters(f -> f.rewritePath("/api/users", "/users"))
-                                .uri("lb://users-service")
-                )
                 .route("users",
-                        pred -> pred.path("/users/**")
-                                .uri("lb://users-service")
+                        pred ->
+                                pred.path("/users/**")
+                                        .uri("lb://users-service")
                 )
+
                 .route("sales",
                         pred -> pred.path("/sales/**")
                                 .uri("lb://sales-service")
                 )
+
                 .route("frontend",
                         pred -> pred.path("/**")
-                                .uri(getFrontEndUri())
+                                .uri(this.frontEndUri)
                 )
+
                 .build();
-    }
-
-    private String getFrontEndUri() {
-        final String FRONTEND_DEFAULT_URI = "http://localhost:1234";
-
-        if (this.frontEndUri != null) {
-            return this.frontEndUri;
-        }
-
-        return FRONTEND_DEFAULT_URI;
     }
 }
