50aac5dfff6c42383e3b96a2de7d05092e3cd1a8
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 20:45:56 -0300

    atualiza: estilo

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,13 +1,15 @@
 import {Link} from "react-router-dom"
 
 const Menu = () => {
+    const cssMenu = "text-sm font-medium text-slate-600 transition-colors hover:text-indigo-600"
+
     return (
         <nav className="flex gap-4">
-            <Link to="/">Home</Link>
-            <Link to="/users">Usuários</Link>
-            <Link to="#">Tickets</Link>
-            <Link to="#">Perfil</Link>
-            <Link to="#">Relatórios</Link>
+            <Link to="/" className={cssMenu}>Home</Link>
+            <Link to="/users" className={cssMenu}>Usuários</Link>
+            <Link to="#" className={cssMenu}>Tickets</Link>
+            <Link to="#" className={cssMenu}>Perfil</Link>
+            <Link to="#" className={cssMenu}>Relatórios</Link>
         </nav>
     )
 }
