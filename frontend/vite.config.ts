446a705059db2b33637f290ad3cd58ea47c3662f
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 13:12:31 -0300

    atualiza: configuração de host do servidor vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    },
+    server: {
+        host: '0.0.0.0'
     }
-  }
 })
