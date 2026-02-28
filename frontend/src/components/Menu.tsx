5d7c71825790b7d6f0507c942a4a8204af28e58c
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 23:07:39 -0300

    atualiza: menu

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,18 +1,19 @@
-import {Link} from "react-router-dom"
-import { navItems } from "@/config/navRoutes"
+import { Link } from "react-router-dom";
+import { navItems } from "@/config/navRoutes";
 
 const Menu = () => {
-    const cssMenu = "text-sm font-medium text-slate-600 transition-colors hover:text-indigo-600"
+	const cssMenu =
+		"text-sm font-medium text-slate-600 transition-colors hover:text-indigo-600";
 
-    return (
-        <nav className="flex gap-4">
-            {navItems.map(item => (
-                <Link key={item.path} to={item.path} className={cssMenu}>
-                    {item.label}
-                </Link>
-            ))}
-        </nav>
-    )
-}
+	return (
+		<nav className="flex gap-4">
+			{navItems.map((item) => (
+				<Link key={item.path} to={item.path} className={cssMenu}>
+					{item.label}
+				</Link>
+			))}
+		</nav>
+	);
+};
 
-export default Menu
+export default Menu;
