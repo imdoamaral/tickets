6023585d30da24675ec65fed81cb3f1f016c7532
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 22:47:10 -0300

    atualiza: router provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import AppRoutes from './routes.tsx'
+import router from './routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppRoutes />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
