11470c38722a0cdf12256051bd1fc624bd57eee3
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 23:07:29 -0300

    adiciona: nav routes

---
+++ frontend/src/config/navRoutes.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+	{ label: "Home", path: "/" },
+	{ label: "Usuários", path: "/users" },
+	{ label: "Criar Evento", path: "/events/create" },
+	{ label: "Vendas", path: "/sales" },
+	{ label: "Criar Venda", path: "/sales/create" },
+	{ label: "Atualizar Status", path: "/sales/update-status" },
+];
