5770f3884d33a215ef1356d4200efd0e17406695
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2026-02-27 23:07:12 -0300

    atualiza: list users

--- frontend/src/pages/users/ListUsers.tsx
+++ frontend/src/pages/users/ListUsers.tsx
@@ -1,43 +1,35 @@
-import api from "@/services/api"
-import type {UserInterface} from "@/types/users"
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 import Card from "@/components/Card.tsx";
-
+import api from "@/services/api";
+import type { UserInterface } from "@/types/users";
 
 const ListUsers = () => {
+	// Hook: useState
+	const [users, setUsers] = useState<UserInterface[]>([]);
 
-    // Hook: useState
-    const [users, setUsers] = useState<UserInterface[]>([])
+	// Hook: useEffect
+	useEffect(() => {
+		api.get("/users").then((response) => {
+			console.log("Response completo:", response);
+			console.log("Response data:", response.data);
+			console.log("Tipo de data:", typeof response.data);
+			setUsers(response.data);
+		});
+	}, []);
 
-    // Hook: useEffect
-    useEffect(() => {
+	return (
+		<div className="flex flex-wrap justify-center">
+			{users.map((user) => (
+				<Card
+					key={user.id}
+					id={user.id}
+					name={user.name}
+					updateUrl={`/users/${user.id}`}
+					deleteUrl={`/users/${user.id}/delete`}
+				/>
+			))}
+		</div>
+	);
+};
 
-        api.get('/users')
-            .then(response => {
-                console.log('Response completo:', response)
-                console.log('Response data:', response.data)
-                console.log('Tipo de data:', typeof response.data)
-                setUsers(response.data)
-            })
-
-    }, [])
-
-    return (
-        <div className="flex flex-wrap justify-center">
-            {
-                users.map(user => (
-                    <Card
-                        key={user.id}
-                        id={user.id}
-                        name={user.name}
-                        updateUrl={`/users/${user.id}`}
-                        deleteUrl={`/users/${user.id}/delete`}
-
-                    />
-                ))
-            }
-        </div>
-    )
-}
-
-export default ListUsers
+export default ListUsers;
